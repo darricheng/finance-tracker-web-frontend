74019d218a07bdf243a7277a8528dce6f1349074
Darric Heng <darricheng@gmail.com>
2023-01-05 23:05:52 +0800

    Add comments for clarity of purpose

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,5 @@
+<!-- Required for using tailwindcss, which itself is a dependency of daisyui -->
+<!-- See: https://tailwindcss.com/docs/guides/sveltekit -->
 <script>
 	import '../app.css';
 </script>
