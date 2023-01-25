e976604d47ce1ac1f0a8d1fe4875a0426f41f10c
Darric Heng <darricheng@gmail.com>
2023-01-25 21:03:13 +0800

    Remove old comments

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -19,22 +19,6 @@
 			<li><a href="/about">About</a></li>
 			<li><a href="/login">Login</a></li>
 		</ul>
-
-		<!-- For when the user is logged in -->
-		<!-- <div class="dropdown dropdown-end">
-      <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-        <div class="w-10 rounded-full">
-          <img src="https://placeimg.com/80/80/people" />
-        </div>
-      </label>
-      <ul tabindex="0" class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-        <li>
-          <a>Profile</a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
-    </div> -->
 	</div>
 </div>
 
