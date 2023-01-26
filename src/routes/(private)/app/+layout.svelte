d7b6d3bcff44d980eedf6222b975816f86ca7129
Darric Heng <darricheng@gmail.com>
2023-01-26 15:40:17 +0800

    Remove profile link

--- src/routes/(private)/app/+layout.svelte
+++ src/routes/(private)/app/+layout.svelte
@@ -40,9 +40,6 @@
 				tabindex="0"
 				class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
 			>
-				<li>
-					<a>Profile</a>
-				</li>
 				<li><a>Settings</a></li>
 				<li><a on:click={logout}>Logout</a></li>
 			</ul>
