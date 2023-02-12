5534d773bf076904d6db4405255c1f23b7a6875e
Darric Heng <darricheng@gmail.com>
2023-02-12 17:47:39 +0800

    Add can remove categories & other misc

--- src/routes/(private)/app/setup/+page.svelte
+++ src/routes/(private)/app/setup/+page.svelte
@@ -6,11 +6,19 @@
 
 	let categories: string[] = [];
 
-	function addNewCategory(e: Event) {
+	function addNewCategory() {
 		// Add the category to the categories array, then clear the input field
 		categories = [...categories, categoryInput];
 		categoryInput = '';
 		console.log(categories);
+	}
+
+	function removeCategory(i: number) {
+		// Remove the category from the categories array
+		return () => {
+			categories = categories.filter((_, index) => index !== i);
+			console.log(categories);
+		};
 	}
 </script>
 
@@ -19,10 +27,14 @@
 		<h1>Setup</h1>
 		<p class="text-2xl">What are your main spending categories?</p>
 		<ul class="text-left px-24">
-			{#each categories as category}
+			{#each categories as category, i (i)}
 				<li class="relative pb-6 mb-6 text-lg border-b border-dotted border-b-gray-400">
 					{category}
-					<span class="btn btn-circle btn-outline m-0 absolute -top-3 right-0">
+					<span
+						class="btn btn-circle btn-outline m-0 absolute -top-3 right-0"
+						on:click={removeCategory(i)}
+						on:keydown={removeCategory(i)}
+					>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							class="h-6 w-6"
@@ -50,14 +62,17 @@
 			<label class="label" for="category-input">
 				<span class="label-text">Input a category</span>
 			</label>
+			<!-- svelte-ignore a11y-autofocus -->
 			<input
 				id="category-input"
 				type="text"
 				placeholder="e.g. Food, Transport"
 				class="input input-bordered w-full max-w-xs"
 				bind:value={categoryInput}
+				autofocus
 			/>
 		</div>
 		<button class="btn btn-accent" id="add-new-category" type="submit">Add</button>
 	</form>
+	<button class="btn btn-primary">Finish</button>
 </section>
