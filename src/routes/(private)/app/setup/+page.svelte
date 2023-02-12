016d6231c9d58cd421bca6e3e0a14981ba5bc686
Darric Heng <darricheng@gmail.com>
2023-02-12 15:26:21 +0800

    Add basic add category function

--- src/routes/(private)/app/setup/+page.svelte
+++ src/routes/(private)/app/setup/+page.svelte
@@ -1,6 +1,46 @@
 <script lang="ts">
-	// TODO: Add a way for the user to add categories
+	// TODO: Add a button beside each category to remove it from the list
 	// TODO: Update the database with the categories
+
+	let categoryInput = '';
+
+	let categories: string[] = [];
+
+	function addNewCategory(e: Event) {
+		// Add the category to the categories array, then clear the input field
+		categories = [...categories, categoryInput];
+		categoryInput = '';
+		console.log(categories);
+	}
 </script>
 
-<h1>Setup</h1>
+<section class="w-full mt-12 flex flex-col space-y-8 items-center">
+	<section class="prose text-center">
+		<h1>Setup</h1>
+		<p>What are your main spending categories?</p>
+		<ul class="text-left">
+			{#each categories as category}
+				<li>{category}</li>
+			{/each}
+		</ul>
+	</section>
+	<form
+		id="new-categories-input"
+		class="flex items-end space-x-4"
+		on:submit|preventDefault={addNewCategory}
+	>
+		<div class="form-control w-full max-w-xs">
+			<label class="label" for="category-input">
+				<span class="label-text">Input a category</span>
+			</label>
+			<input
+				id="category-input"
+				type="text"
+				placeholder="e.g. Food, Transport"
+				class="input input-bordered w-full max-w-xs"
+				bind:value={categoryInput}
+			/>
+		</div>
+		<button class="btn btn-accent" id="add-new-category" type="submit">Add</button>
+	</form>
+</section>
