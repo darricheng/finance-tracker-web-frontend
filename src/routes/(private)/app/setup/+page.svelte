949177c29f53b67360bf2ecbd2c8325b786490b6
Darric Heng <darricheng@gmail.com>
2023-02-12 16:36:28 +0800

    Display a button for each added category

--- src/routes/(private)/app/setup/+page.svelte
+++ src/routes/(private)/app/setup/+page.svelte
@@ -17,10 +17,27 @@
 <section class="w-full mt-12 flex flex-col space-y-8 items-center">
 	<section class="prose text-center">
 		<h1>Setup</h1>
-		<p>What are your main spending categories?</p>
-		<ul class="text-left">
+		<p class="text-2xl">What are your main spending categories?</p>
+		<ul class="text-left px-24">
 			{#each categories as category}
-				<li>{category}</li>
+				<li class="relative pb-6 mb-6 text-lg border-b border-dotted border-b-gray-400">
+					{category}
+					<span class="btn btn-circle btn-outline m-0 absolute -top-3 right-0">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+							><path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M6 18L18 6M6 6l12 12"
+							/></svg
+						>
+					</span>
+				</li>
 			{/each}
 		</ul>
 	</section>
