71480bad50e8e7fe668ed223399bd9330cfc1a98
Darric Heng <darricheng@gmail.com>
2023-02-20 21:27:57 +0800

    Add settings page

--- src/routes/(private)/app/+layout.svelte
+++ src/routes/(private)/app/+layout.svelte
@@ -40,7 +40,7 @@
 				tabindex="0"
 				class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
 			>
-				<li><a>Settings</a></li>
+				<li><a href="/app/settings">Settings</a></li>
 				<li><a on:click={logout}>Logout</a></li>
 			</ul>
 		</div>
